71a89734ddd5f43ef93d1a0d9d9e9ffb3260e531
psb6396 <psb6396@naver.com>
2024-10-24 18:23:23 +0900

    52

--- Javascript_study-github/javascript_class/Javascript_Quiz/부록. 디버깅/quiz1/quiz1.js
+++ Javascript_study-github/javascript_class/Javascript_Quiz/부록. 디버깅/quiz1/quiz1.js
@@ -3,6 +3,6 @@ let sum = 0
 for (let i = 1; i <= 100; i++) {
    sum += i * 10
    console.log('덧셈중..')
-}
+} //i=33일때 sum = 5280
 
 console.log('합계', sum)
--- Javascript_study-github/javascript_class/Javascript_Quiz/부록. 디버깅/quiz3/quiz3.js
+++ Javascript_study-github/javascript_class/Javascript_Quiz/부록. 디버깅/quiz3/quiz3.js
@@ -2,7 +2,7 @@ const obj = {
    0: '10',
    1: '20',
 }
-
+console.log(typeof obj)
 for (let val of obj) {
    console.log(val)
 }
