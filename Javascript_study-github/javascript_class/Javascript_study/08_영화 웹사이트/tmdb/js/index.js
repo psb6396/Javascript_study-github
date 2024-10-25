52b71e8ae5c1f830deac103c70e9a3f6c94bc916
psb6396 <psb6396@naver.com>
2024-10-25 16:51:29 +0900

    f

--- Javascript_study-github/javascript_class/Javascript_study/08_영화 웹사이트/tmdb/js/index.js
+++ Javascript_study-github/javascript_class/Javascript_study/08_영화 웹사이트/tmdb/js/index.js
@@ -41,11 +41,11 @@ const getPlayingMovies = async (url) => {
             rowHtml += `<div class="col-sm-3 p-3">
                      <div class="card">
                         <a href="./detail.html?movie_id=${movie.id}">
-                           <img src="..." class="card-img-top poster" alt="${movie.title}" />
+                           <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top poster" alt="${movie.title}" />
                         </a>
                         <div class="card-body">
                            <p class="card-text title">${movie.title}</p>
-                           <p class="card-text average">${movie.vote_aveage.toFixed(1)}</p>
+                           <p class="card-text average">${movie.vote_average.toFixed(1)}</p>
                         </div>
                      </div>
                   </div>`
